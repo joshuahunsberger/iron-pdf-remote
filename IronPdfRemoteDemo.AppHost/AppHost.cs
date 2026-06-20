48bf947104c91078e90e0f1af79f3cb530e2b5c5
Joshua Hunsberger <joshua@hunsberger.co>
2026-06-20 08:03:35 -0400

    Populate IronPDF license key from Aspire parameter

--- IronPdfRemoteDemo.AppHost/AppHost.cs
+++ IronPdfRemoteDemo.AppHost/AppHost.cs
@@ -1,5 +1,7 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+var ironPdfLicenseKey = builder.AddParameter("IronPdfLicenseKey");
+
 var engine = builder.AddContainer("ironpdfengine", "ironsoftwareofficial/ironpdfengine", "2026.6.1")
     .WithEndpoint(targetPort: 33350, name: "grpc", scheme: "http");
 
@@ -8,6 +10,7 @@ var grpcEndpoint = engine.GetEndpoint("grpc");
 builder.AddProject<Projects.Worker>("worker")
     .WithEnvironment("IRONPDF_ENGINE_HOST", grpcEndpoint.Property(EndpointProperty.Host))
     .WithEnvironment("IRONPDF_ENGINE_PORT", grpcEndpoint.Property(EndpointProperty.Port))
+    .WithEnvironment("IRONPDF_LICENSE_KEY", ironPdfLicenseKey)
     .WaitFor(engine);
 
 builder.Build().Run();
--- Worker/Program.cs
+++ Worker/Program.cs
@@ -3,11 +3,12 @@ using IronPdf.GrpcLayer;
 
 var engineHost = Environment.GetEnvironmentVariable("IRONPDF_ENGINE_HOST") ?? "localhost";
 var enginePort = int.TryParse(Environment.GetEnvironmentVariable("IRONPDF_ENGINE_PORT"), out var p) ? p : 33350;
+var licenseKey = Environment.GetEnvironmentVariable("IRONPDF_LICENSE_KEY");
 
 var config = IronPdfConnectionConfiguration.RemoteServer($"http://{engineHost}:{enginePort}");
 
 Installation.ConnectToIronPdfHost(config);
-Installation.LicenseKey = "PLACEHOLDER";
+Installation.LicenseKey = licenseKey;
 
 var builder = Host.CreateApplicationBuilder(args);
 builder.AddServiceDefaults();
