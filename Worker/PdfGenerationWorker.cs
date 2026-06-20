ea8a15a389985bf78beb2104a405d262fae8776c
Joshua Hunsberger <joshua@hunsberger.co>
2026-06-20 08:16:43 -0400

    Use async render method

--- Worker/PdfGenerationWorker.cs
+++ Worker/PdfGenerationWorker.cs
@@ -2,16 +2,14 @@ namespace Worker;
 
 public class PdfGenerationWorker(ILogger<PdfGenerationWorker> logger, IHostApplicationLifetime hostApplicationLifetime) : BackgroundService
 {
-    protected override Task ExecuteAsync(CancellationToken stoppingToken)
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
         var renderer = new ChromePdfRenderer();
-        var pdf = renderer.RenderHtmlAsPdf("<h1>Hello World!</h1>");
+        var pdf = await renderer.RenderHtmlAsPdfAsync("<h1>Hello World!</h1>");
         logger.LogInformation("PDF rendered");
         pdf.SaveAs("pdfOutput/ironpdf.pdf");
         logger.LogInformation("PDF saved");
         
         hostApplicationLifetime.StopApplication();
-
-        return Task.CompletedTask;
     }
 }
