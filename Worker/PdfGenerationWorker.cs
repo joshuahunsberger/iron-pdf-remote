759ed251b61b07772d719e6f541776c9c28d324c
Joshua Hunsberger <joshua@hunsberger.co>
2026-06-20 09:35:52 -0400

    Include timestamp in PDF generation

--- Worker/PdfGenerationWorker.cs
+++ Worker/PdfGenerationWorker.cs
@@ -5,7 +5,8 @@ public class PdfGenerationWorker(ILogger<PdfGenerationWorker> logger, IHostAppli
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
         var renderer = new ChromePdfRenderer();
-        var pdf = await renderer.RenderHtmlAsPdfAsync("<h1>Hello World!</h1>");
+        var now = DateTime.UtcNow;
+        var pdf = await renderer.RenderHtmlAsPdfAsync($"<h1>Hello World!</h1><h3>Generated at {now:O}</h3>");
         logger.LogInformation("PDF rendered");
         pdf.SaveAs("pdfOutput/ironpdf.pdf");
         logger.LogInformation("PDF saved");
